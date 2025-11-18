8a2597d4ed2d4414fd6ee055f1bbd2c0dd6bc21f
Marcio Santos <marciosanthos97@gmail.com>
2025-11-18 19:35:42 -0300

    Primeiro parte: Criamos um novo serviço.
    segundo: Criamos uma nova rota e registramos a mesma rota.

--- src/app.py
+++ src/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from routes import users_bp 
+from controllers.exchange_controller import exchange_bp
+
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(users_bp)
+    app.register_blueprint(exchange_bp)
     return app
 
 if __name__ == "__main__":
---
+++ src/controllers/exchange_controller.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, jsonify
+from services.exchange_service import get_usd_to_brl
+
+exchange_bp = Blueprint("exchange", __name__)
+
+@exchange_bp.route("/exchange/usd-to-brl", methods=["GET"])
+def usd_to_brl():
+    try:
+        rate = get_usd_to_brl()
+        return jsonify(rate), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
